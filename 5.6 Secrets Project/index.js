8b12463cb1a0da000bf2354da5a1318278406341
ROSIEANNA <tranqcuong941@gmail.com>
2024-07-10 18:22:37 +0700

    5.6

---
+++ 5.6 Secrets Project/index.js
@@ -0,0 +1,23 @@
+import express from "express";
+import axios from "axios"
+
+const app = express();
+const port = 3000;
+
+app.use(express.static("public"));
+
+app.get("/", async (req, res) => {
+    try {
+      const result = await axios.get("https://secrets-api.appbrewery.com/random");
+      res.render("index.ejs", { 
+        secret: result.data.secret,
+        user: result.data.username,
+     });
+    } catch (error) {
+    }
+})
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+  });
+  
