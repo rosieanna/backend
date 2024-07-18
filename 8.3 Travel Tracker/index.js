5142f85f55a5090cae00c084f2f1c3127163adc4
Rosieanna <tranqcuong941@gmail.com>
2024-07-18 17:40:51 +0700

    8.3 part 1

---
+++ 8.3 Travel Tracker/index.js
@@ -0,0 +1,33 @@
+import express from "express";
+import bodyParser from "body-parser";
+import pg from "pg";
+
+const db = new pg.Client({
+  user: "postgres",
+  host: "localhost",
+  database: "world",
+  password: "a",
+  port: 5432
+});
+db.connect();
+
+const app = express();
+const port = 3000;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+app.get("/", async (req, res) => {
+  //Write your code here.
+  const result = await db.query("select * from visited_countries");
+  let countries = [];
+  result.rows.forEach((country) => {
+    countries.push(country.country_code);
+  });
+  res.render("index.ejs", { countries: countries, total: countries.length });
+  db.end();
+});
+
+app.listen(port, () => {
+  console.log(`Server running on http://localhost:${port}`);
+});
